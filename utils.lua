fe644a6961210cbeda56f561000708a1a02e32f6
Deleah Lenaya <lenayadeleahnc@hotmail.com>
2026-08-26 09:32:58 +0200

    update logger setup with file rotation

--- utils.lua
+++ utils.lua
@@ -1,33 +1,34 @@
 local utils = {}
+local max_size = 1024 * 50
 
-function utils.setup_logger(filepath, max_size_bytes)
-    max_size_bytes = max_size_bytes or 1024 * 1024
-    
-    local function get_size(path)
-        local file = io.open(path, "r")
-        if not file then return 0 end
-        local size = file:seek("end")
-        file:close()
-        return size
-    end
+function utils.get_file_size(path)
+    local file = io.open(path, "r")
+    if not file then return 0 end
+    local size = file:seek("end")
+    file:close()
+    return size
+end
 
-    local function rotate()
-        os.remove(filepath .. ".old")
-        os.rename(filepath, filepath .. ".old")
-    end
+function utils.rotate_logs(base_path)
+    if utils.get_file_size(base_path) < max_size then return end
+    local old_path = base_path .. ".old"
+    os.remove(old_path)
+    os.rename(base_path, old_path)
+end
 
-    return function(level, message)
-        if get_size(filepath) >= max_size_bytes then
-            rotate()
+function utils.setup_logger(filename)
+    local path = filename or "clicker.log"
+    return {
+        log = function(self, level, msg)
+            utils.rotate_logs(path)
+            local file = io.open(path, "a")
+            if file then
+                local entry = string.format("[%s] [%s] %s\n", os.date("%Y-%m-%d %H:%M:%S"), level, msg)
+                file:write(entry)
+                file:close()
+            end
         end
-        
-        local log_file = io.open(filepath, "a")
-        if log_file then
-            local timestamp = os.date("%Y-%m-%d %H:%M:%S")
-            log_file:write(string.format("[%s] [%s] %s\n", timestamp, level, message))
-            log_file:close()
-        end
-    end
+    }
 end
 
 return utils

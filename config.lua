f07be0926d9132615aab05a03c312454cf56a5df
Deleah Lenaya <lenayadeleahnc@hotmail.com>
2026-08-26 09:30:40 +0200

    configuration loader with default values

--- config.lua
+++ config.lua
@@ -2,19 +2,20 @@ local config = {}
 
 local defaults = {
     cps = 15,
-    hotkey = "F6",
-    toggle_sound = true,
-    randomization = 0.15,
-    click_type = "left"
+    key = "F6",
+    jitter = false,
+    randomize = true,
+    sound = "click.wav"
 }
 
 function config.load(user_path)
     local loaded = {}
-    local file = io.open(user_path or "settings.dat", "r")
+    setmetatable(loaded, { __index = defaults })
     
+    local file = io.open(user_path, "r")
     if file then
         for line in file:lines() do
-            local k, v = line:match("^%s*(%w+)%s*=%s*(.+)%s*$")
+            local k, v = line:match("^%s*(.-)%s*=%s*(.-)%s*$")
             if k and v then
                 if v == "true" then v = true
                 elseif v == "false" then v = false
@@ -25,12 +26,6 @@ function config.load(user_path)
         end
         file:close()
     end
-    
-    setmetatable(loaded, {
-        __index = function(_, key)
-            return defaults[key]
-        end
-    })
     
     return loaded
 end

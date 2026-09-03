f1eefb00949eb76716077905d82d84986f43b696
Deleah Lenaya <lenayadeleahnc@hotmail.com>
2026-09-03 09:05:59 +0200

    persistent configuration management and serialization logic

--- config.lua
+++ config.lua
@@ -1,76 +1,40 @@
-local config = {}
-config.logger = {
-    file = "auto-clicker-95.log",
-    max_size = 1024 * 1024,
-    max_backups = 5,
-    level = "INFO"
+-- auto-clicker-95 configuration management
+local json = require('json')
+
+local ConfigManager = {
+    storage_path = 'settings.dat',
+    defaults = {
+        cps = 10,
+        toggle_key = 'f6',
+        jitter_radius = 2
+    }
 }
 
-local log_levels = { DEBUG = 1, INFO = 2, WARN = 3, ERROR = 4 }
-
-function config.logger:should_log(level)
-    return log_levels[level] >= log_levels[self.level]
+function ConfigManager.load()
+    local file = io.open(ConfigManager.storage_path, 'r')
+    if not file then return ConfigManager.defaults end
+    
+    local content = file:read('*all')
+    file:close()
+    
+    local ok, data = pcall(json.decode, content)
+    return ok and data or ConfigManager.defaults
 end
 
-function config.logger:rotate()
-    for i = self.max_backups, 2, -1 do
-        local src = self.file .. "." .. (i - 1)
-        local dst = self.file .. "." .. i
-        local handle = io.open(src, "r")
-        if handle then
-            handle:close()
-            pcall(os.remove, dst)
-            os.rename(src, dst)
-        end
-    end
-    local handle = io.open(self.file, "r")
-    if handle then
-        handle:close()
-        pcall(os.remove, self.file .. "." .. (self.max_backups + 1))
-        os.rename(self.file, self.file .. ".1")
+function ConfigManager.save(data)
+    local file = io.open(ConfigManager.storage_path, 'w')
+    if file then
+        file:write(json.encode(data, {indent = true}))
+        file:close()
     end
 end
 
-function config.logger:write_log(level, msg)
-    if not self:should_log(level) then return end
-    local timestamp = os.date("%Y-%m-%d %H:%M:%S")
-    local log_entry = string.format("%s [%s] %s\n", timestamp, level, msg)
-    local handle = io.open(self.file, "a")
-    if handle then
-        handle:write(log_entry)
-        handle:close()
+function ConfigManager.apply_patch(patch)
+    local current = ConfigManager.load()
+    for k, v in pairs(patch) do
+        current[k] = v
     end
-    handle = io.open(self.file, "r")
-    if handle then
-        local size = handle:seek("end")
-        handle:close()
-        if size > self.max_size then
-            self:rotate()
-        end
-    end
+    ConfigManager.save(current)
 end
 
-local proxy = {}
-setmetatable(proxy, {
-    __index = function(t, level)
-        return function(msg)
-            config.logger:write_log(level, msg)
-        end
-    end
-})
-
-config.log = proxy
-
-function config.setup_logger(settings)
-    if settings then
-        for k, v in pairs(settings) do
-            if config.logger[k] ~= nil then
-                config.logger[k] = v
-            end
-        end
-    end
-    local h = io.open(config.logger.file, "a")
-    if h then h:close() end
-end
-
-return config
+return ConfigManager

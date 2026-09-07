f2238cc4103e2279d7197bad8ae02d1990e2360a
Deleah Lenaya <lenayadeleahnc@hotmail.com>
2026-09-07 09:07:47 +0200

    ffi integration for low latency clicking

--- helpers.lua
+++ helpers.lua
@@ -1,34 +1,33 @@
+local ffi = require('ffi')
+ffi.cdef[[
+    typedef struct { long x; long y; } point_t;
+    void SetCursorPos(int x, int y);
+    void mouse_event(int flags, int dx, int dy, int data, unsigned long extra);
+]]
+
+local user32 = ffi.load('user32')
+local MOUSEEVENTF_LEFTDOWN = 0x0002
+local MOUSEEVENTF_LEFTUP = 0x0004
+
 local helpers = {}
 
-function helpers.serialize(data)
-    local chunks = {'return {'}
-    for k, v in pairs(data) do
-        local val = type(v) == 'string' and string.format('%q', v) or tostring(v)
-        table.insert(chunks, string.format('  [%q] = %s,', tostring(k), val))
+function helpers.optimized_click(x, y)
+    user32.SetCursorPos(x, y)
+    user32.mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+    user32.mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+end
+
+local click_cache = {}
+function helpers.memoized_coords(x, y)
+    local key = string.format('%d:%d', x, y)
+    if not click_cache[key] then
+        click_cache[key] = {x = x, y = y}
     end
-    table.insert(chunks, '}')
-    return table.concat(chunks, '\n')
+    return click_cache[key]
 end
 
-function helpers.deserialize(raw)
-    local func, err = load(raw)
-    if not func then return nil, err end
-    return func()
-end
-
-function helpers.sanitize_path(path)
-    local clean = string.gsub(path, '[^%w%./_-]', '')
-    return './data/' .. clean .. '.lua'
-end
-
-function helpers.save_profile(name, cfg)
-    local file = io.open(helpers.sanitize_path(name), 'w')
-    if file then
-        file:write(helpers.serialize(cfg))
-        file:close()
-        return true
-    end
-    return false
+function helpers.clear_cache()
+    click_cache = {}
 end
 
 return helpers

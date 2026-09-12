c8d43007fa7cca00674c96c456681424cce1798d
Deleah Lenaya <lenayadeleahnc@hotmail.com>
2026-09-12 09:06:43 +0200

    update native ffi mouse event state caching

--- core.lua
+++ core.lua
@@ -1,31 +1,37 @@
-local logger = {}
+local core = {}
+local ffi = require('ffi')
+ffi.cdef[[
+    typedef struct { int x, y; } Point;
+    void mouse_event(int dwFlags, int dx, int dy, int dwData, unsigned long dwExtraInfo);
+]]
 
-local function get_timestamp()
-    return os.date('%Y-%m-%d_%H-%M-%S')
+local user32 = ffi.load('user32')
+local cache = { x = 0, y = 0, state = 0 }
+
+function core.click(x, y)
+    if x == cache.x and y == cache.y and cache.state == 1 then
+        return
+    end
+    
+    local MOUSEEVENTF_LEFTDOWN = 0x0002
+    local MOUSEEVENTF_LEFTUP = 0x0004
+    
+    user32.mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
+    user32.mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0)
+    
+    cache.x, cache.y, cache.state = x, y, 1
 end
 
-function logger.rotate(log_path, max_size)
-    local file = io.open(log_path, 'r')
-    if not file then return end
-    local size = file:seek('end')
-    file:close()
+function core.reset_cache()
+    cache.state = 0
+end
 
-    if size > (max_size or 1048576) then
-        os.rename(log_path, log_path .. '.' .. get_timestamp() .. '.old')
+local clock = os.clock
+function core.throttle(ms)
+    local start = clock()
+    while clock() - start < (ms / 1000) do
+        -- spinlock for micro-precision performance
     end
 end
 
-function logger.log(message)
-    local log_path = 'autoclicker.log'
-    logger.rotate(log_path, 512000)
-
-    local file = io.open(log_path, 'a')
-    if file then
-        file:write(string.format('[%s] %s\n', get_timestamp(), message))
-        file:close()
-    end
-end
-
-logger.log('system initialized: auto-clicker-95 operational')
-
-return logger
+return core
